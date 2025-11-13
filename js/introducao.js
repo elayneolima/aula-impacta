37f8c7aaa008d7b63fbcd7352fdbac0a76283f15
elayneoslima'git config --global user.email <elayne04lima@gmail.com>
2025-11-13 19:22:30 -0300

    aula 2 de javascript

---
+++ js/introducao.js
@@ -0,0 +1,40 @@
+// alert('Hello World!')
+
+// console.log('hello, wolrd!');
+
+
+// var nome; 
+
+
+// nome = "Lana";
+
+
+// var valorCurso = 550.42; 
+// console.log(valorCurso + 10.00);
+
+// var idade = 10; 
+// console.log(++idade);
+
+
+
+
+// pagina estatica: pagina sem interação
+// usado muito para debugar um código
+// Js não é tipado, como ele sabe o tipo? A partir do valor que voce colocar ele entende qual tipo é. 
+
+//typeof diz qual o tipo da variavel
+
+/**
+String: Cadeia de Caracteres que fica dentro de aspas
+Number: Numero
+Boolean: tipo true or false, tipo lógico
+Null: Quando queremos EXPLICITAMENTE dizer ao javascript que ela nao tem valor. A variavel é definida como objetct
+Undefined: quando a variavel não está recebendo nada. Nesse caso, ela foi criada mas nao foi atribuido nenhum valor.
+ */
+
+
+// = é sinal para atribuição
+//Javasc não exige que vc informe qual o tipo de informação que voce quer adicionar na variavel
+// o ponto e virgula não é exigido para finalizar um comando
+//armazena um espaço na memoria e guarda uma informação que varia
+// variavel nunca começa com numeros, pode começar com $ ou _ 
