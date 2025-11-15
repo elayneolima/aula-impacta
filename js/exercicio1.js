58e65493a6fb4fe27699fd0c56b76a3c5893b98f
elayneoslima'git config --global user.email <elayne04lima@gmail.com>
2025-11-15 14:22:32 -0300

    ajustando layout da primeira seção

--- js/exercicio1.js
+++ js/exercicio1.js
@@ -1,58 +1,59 @@
 
 
-(function (calc) {
-    let interromper = true;
-    while(interromper) {
-        const imposto = 0.2;
-        const produtos = [
-            ['Sabão', 2.00], // cod 1
-            ['Leite', 5.00], // cod 2
-            ['Açucar',5.50], // cod 3
-        ];
-        const dados = [];
-        const mensagens = [
-            'codigo do produto',
-            'digite a quantidade',
-        ];
+// (function (calc) {
+//     let interromper = true;
+//     while(interromper) {
+//         const imposto = 0.2;
+//         const produtos = [
+//             ['Sabão', 2.00], // cod 1
+//             ['Leite', 5.00], // cod 2
+//             ['Açucar',5.50], // cod 3
+//         ];
+//         const dados = [];
+//         const mensagens = [
+//             'codigo do produto',
+//             'digite a quantidade',
+//         ];
 
-        // alert('produto não existe');
+//         // alert('produto não existe');
     
-        for(let i = 0; i < mensagens.length; i++) {
-            let dado = prompt(mensagens[i]) 
-            dados.push(dado);
-        }
+//         for(let i = 0; i < mensagens.length; i++) {
+//             let dado = prompt(mensagens[i]) 
+//             dados.push(dado);
+//         }
 
-        const cod = dados[0];
-        const produtoSelecionado = produtos[cod - 1]
+//         const cod = dados[0];
+//         const produtoSelecionado = produtos[cod - 1]
 
-        if(!produtoSelecionado){
-         alert("Esse produto não existe!");
-        }
+//         if(!produtoSelecionado){
+//          alert("Esse produto não existe!");
+//          break
+//         }
 
-        const totalCompra = calc(dados[1], produtoSelecionado[1], imposto);
+//         const totalCompra = calc(dados[1], produtoSelecionado[1], imposto);
 
      
 
-        console.log(`Produto:  ${produtoSelecionado[0]}`);
-        console.log(`Valor Unitario: ${produtoSelecionado[1]}`);
-        console.log(`Total da compra: ${totalCompra}`);
-        console.log(`Imposto cobrado: 20%`);
+//         console.log(`Produto:  ${produtoSelecionado[0]}`);
+//         console.log(`Valor Unitario: ${produtoSelecionado[1]}`);
+//         console.log(`Total da compra: ${totalCompra}`);
+//         console.log(`Imposto cobrado: 20%`);
 
-        interromper = confirm('Deseja continuar');
-    }
-
-
-})(calculoTotalCompra)
+//         interromper = confirm('Deseja continuar');
+//     }
 
 
+// })(calculoTotalCompra)
 
-function calculoTotalCompra(quantidade, valorProduto, imposto = null) {
-    const totalCompraSemImposto = quantidade * valorProduto;
-    if (imposto) {
-        const total = totalCompraSemImposto + totalCompraSemImposto * imposto;
-        return total;
-    }
-    return totalCompraSemImposto;
-}
+
+
+// function calculoTotalCompra(quantidade, valorProduto, imposto = null) {
+//     const totalCompraSemImposto = quantidade * valorProduto;
+//     if (imposto) {
+//         const total = totalCompraSemImposto + totalCompraSemImposto * imposto;
+//         return total;
+//     }
+//     return totalCompraSemImposto;
+// }
 
    
